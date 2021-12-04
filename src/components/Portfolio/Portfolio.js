5123c36fe62fb1b913fb52d0b750bd22afc53d68
HossenMahmud <hossenmahmud15@gmail.com>
2021-12-05 00:11:50 +0600

    update portfolio

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import './Portfolio.css';
-import work1 from '../../images/work/work1.png';
-import work2 from '../../images/work/work2.png';
-import work3 from '../../images/work/work3.png';
+import work1 from '../../images/work/work11.png';
+import work2 from '../../images/work/work22.png';
+import work3 from '../../images/work/work33.png';
+import work4 from '../../images/work/work4.png';
+import work5 from '../../images/work/work5.png';
+import work6 from '../../images/work/work6.png';
 const Portfolio = () => {
     return (
         <div id='portfolio' className='portfolio-bg'>
@@ -59,6 +62,63 @@ const Portfolio = () => {
                                     <div className="links">
                                         <a rel="noopener noreferrer" href="https://hardcore-tereshkova-19b4ad.netlify.app/" className="link" target="_blank"><i className="fas fa-link"></i></a>
                                         <a rel="noopener noreferrer" className="img-popup image-preview" target="_blank" href="https://github.com/HossenMahmud/kitton-app">
+                                            <i className="fab fa-github"></i>
+                                        </a>
+                                    </div>
+                                    <div className='details-button'>
+                                        <button>Project Details</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-4 mb-4">
+                        <div className="item-thumbnail">
+                            <img className='img-fluid' src={work4} alt="" />
+                            <div className="content-overlay">
+                                <div className="content">
+                                    <h5 className='mt-5 fw-bold'>Suissand Watch Store</h5>
+                                    <div className="links">
+                                        <a rel="noopener noreferrer" href="https://suissand-watch.web.app/" className="link" target="_blank"><i className="fas fa-link"></i></a>
+                                        <a rel="noopener noreferrer" className="img-popup image-preview" target="_blank" href="https://github.com/HossenMahmud/suissand-watch-client">
+                                            <i className="fab fa-github"></i>
+                                        </a>
+                                    </div>
+                                    <div className='details-button'>
+                                        <button>Project Details</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-4 mb-4">
+                        <div className="item-thumbnail">
+                            <img className='img-fluid' src={work5} alt="" />
+                            <div className="content-overlay">
+                                <div className="content">
+                                    <h5 className='mt-5 fw-bold'>Foodball Player</h5>
+                                    <div className="links">
+                                        <a rel="noopener noreferrer" href="https://hossenmahmud.github.io/responsive-football/" className="link" target="_blank"><i className="fas fa-link"></i></a>
+                                        <a rel="noopener noreferrer" className="img-popup image-preview" target="_blank" href="https://github.com/HossenMahmud/responsive-football">
+                                            <i className="fab fa-github"></i>
+                                        </a>
+                                    </div>
+                                    <div className='details-button'>
+                                        <button>Project Details</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-4 mb-4">
+                        <div className="item-thumbnail">
+                            <img className='img-fluid' src={work6} alt="" />
+                            <div className="content-overlay">
+                                <div className="content">
+                                    <h5 className='mt-5 fw-bold'>datasoft company</h5>
+                                    <div className="links">
+                                        <a rel="noopener noreferrer" href="https://youthful-knuth-917d91.netlify.app/" className="link" target="_blank"><i className="fas fa-link"></i></a>
+                                        <a rel="noopener noreferrer" className="img-popup image-preview" target="_blank" href="https://github.com/HossenMahmud/datasoft-company">
                                             <i className="fab fa-github"></i>
                                         </a>
                                     </div>
